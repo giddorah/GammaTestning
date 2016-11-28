9f6a52b71d7ca7548ee1063e86c8c5eeaf6450d3
Michlewood <michlewood@hotmail.com>
2016-11-28 10:34:55 +0100

    ändrade square root

--- GammaProjekt/Runtime.cs
+++ GammaProjekt/Runtime.cs
@@ -95,21 +95,7 @@ namespace GammaProjekt
 
         public double SquareRoot(double input)
         {
-            if (input <= 0.0)
-            {
-                Console.SetCursorPosition(3, 2);
-                Console.WriteLine("        NaN");
-                Console.ReadKey(true);
-            }
-
-            double result = input;
-            double previousResult = -input;
-            while (Math.Abs(previousResult - result) > result / 1000)
-            {
-                previousResult = result;
-                result = (result + input / result) / 2;
-            }
-            return result;
+            return Math.Sqrt(input);
         }
 
         private void ChangeOperator(string newOperator)
