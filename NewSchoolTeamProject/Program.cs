eaf9ba89a29389c83d1eaa88e160c6832ac349f0
giddorah <j.spanberg@live.com>
2017-01-13 13:55:27 +0100

    Initial commit

---
+++ NewSchoolTeamProject/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+/*
+Skapa ett program som skriver ut nummer 1-100
+Om numret är delbart med 3 skriv ut Coca istället
+Om numret är delbart med 5 skriv Cola istället
+Om numret är delbart med 3 och 5 skriv Cocacola istället
+Jobba i grupp mellan 2-3 pers/grupp.
+Ni ska jobba enligt TDD metodik.
+Projektet skall vara väl dokumenterat med kommentarer.
+Deadline för inlämning 25 Januari innan kl 00:00
+*/
+
+
+namespace NewSchoolTeamProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
