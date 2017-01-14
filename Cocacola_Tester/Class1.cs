916bcf5e8a985935e7d8b0589af9ece752761dd3
Michlewood <michlewood@hotmail.com>
2017-01-14 13:29:22 +0100

    Skapat classerna och metoderna och gjort första testet

---
+++ Cocacola_Tester/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CocacolaCalculator;
+namespace Cocacola_Tester
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test_If_Input_15_Returns_Cocacola()
+        {
+            CocacolaCal ccc = new CocacolaCal();
+            int input = 15;
+            string expectedOutput = "Cocacola";
+            string actualOutput = ccc.CheckIfDivisibleBy3And5(input);
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+
+    }
+}
