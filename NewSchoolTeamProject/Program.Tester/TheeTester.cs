ec1dde0c84e7bfa57f5c415a2a55ae4597071cb2
giddorah <j.spanberg@live.com>
2017-01-17 14:02:39 +0100

    Added files.

---
+++ NewSchoolTeamProject/Program.Tester/TheeTester.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Program.Library;
+
+namespace Program.Tester
+{
+    [TestClass]
+    public class TheeTester
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
