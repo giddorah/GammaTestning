ce74d9c1446bab0008ea4efca656835060a4e301
giddorah <j.spanberg@live.com>
2017-01-17 13:48:26 +0100

    Initial

---
+++ NewSchoolTeamProject/Three.Tester/Three.Tester.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Three.Tester
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SeeIfDividableByThreeReturnsCoca()
+        {
+        }
+
+        [TestMethod]
+        public void SeeSoItDoesNotReturnAnythingElse()
+        {
+            
+        }
+    }
+}
